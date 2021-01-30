e4348e44664855f058da0fc07d229d435309417e
naerim <naerim1119@gmail.com>
2021-01-31 04:27:26 +0900

    [ADD] 회원가입 페이지 데모

--- app/src/main/java/com/example/dailygreen_app/LoginActivity.kt
+++ app/src/main/java/com/example/dailygreen_app/LoginActivity.kt
@@ -1,12 +1,24 @@
 package com.example.dailygreen_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var btn_login : Button
+    lateinit var btn_signUp : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btn_login = findViewById(R.id.btn_login)
+        btn_signUp = findViewById(R.id.btn_signUp)
+
+        btn_signUp.setOnClickListener {
+            startActivity(Intent(this,SignUpActivity::class.java))
+        }
 
     }
 }
