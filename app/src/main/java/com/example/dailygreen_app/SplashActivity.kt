0ba72bda6d6c7a32639c1ff5e699b1dd0a7f4bc0
naerim <naerim1119@gmail.com>
2021-02-02 00:22:24 +0900

    [ADD] 스플래시 화면 완성

---
+++ app/src/main/java/com/example/dailygreen_app/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.example.dailygreen_app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+
+class SplashActivity : AppCompatActivity() {
+    private val SPLASH_TIME : Long = 2000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // 하단바 제거
+        window.decorView.apply {
+            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        }
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        },SPLASH_TIME)
+    }
+}
