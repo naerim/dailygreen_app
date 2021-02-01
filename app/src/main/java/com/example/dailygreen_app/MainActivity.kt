9b59da11490ef8fca75ada2843adc16d26d55f4b
naerim <naerim1119@gmail.com>
2021-02-02 02:13:17 +0900

    [ADD] 메뉴 프래그먼트 생성

--- app/src/main/java/com/example/dailygreen_app/MainActivity.kt
+++ app/src/main/java/com/example/dailygreen_app/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.dailygreen_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dailygreen_app.menu.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bottom_navigation: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottom_navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottom_navigation.setOnNavigationItemSelectedListener { it ->
+            when (it.itemId) {
+                R.id.action_home -> {
+                    val homeFragment = HomeFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.main_content, homeFragment).commit()
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.action_diary -> {
+                    val diaryFragment = DiaryFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.main_content, diaryFragment).commit()
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.action_alarm -> {
+                    val alarmFragment = AlarmFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.main_content, alarmFragment).commit()
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.action_calender -> {
+                    val calendarFragment = CalenderFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.main_content, calendarFragment).commit()
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.action_recommend -> {
+                    val recommendFragment = RecommendFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.main_content, recommendFragment).commit()
+                    return@setOnNavigationItemSelectedListener true
+                }
+            }
+            return@setOnNavigationItemSelectedListener false
+        }
     }
+
 }
---
+++ app/src/main/java/com/example/dailygreen_app/menu/DiaryFragment.kt
@@ -0,0 +1,19 @@
+package com.example.dailygreen_app.menu
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.dailygreen_app.R
+
+class DiaryFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_diary, container, false)
+        return view
+    }
+}
