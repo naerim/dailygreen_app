8d4bc84b4da28b6d61675dc433aae87dcc454aad
naerim <naerim1119@gmail.com>
2021-02-03 01:38:46 +0900

    [ADD] 알람 다이얼로그 생성, 기능만 구현

--- app/src/main/java/com/example/dailygreen_app/menu/AlarmFragment.kt
+++ app/src/main/java/com/example/dailygreen_app/menu/AlarmFragment.kt
@@ -1,19 +1,46 @@
 package com.example.dailygreen_app.menu
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.dailygreen_app.R
 
 class AlarmFragment : Fragment(){
+    lateinit var set_alarm : Button
+    lateinit var set_textView : TextView
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_alarm, container, false)
+
+        set_alarm = view.findViewById(R.id.set_alarm)
+        set_textView = view.findViewById(R.id.set_textView)
+
+        set_alarm.setOnClickListener {
+            showDialog()
+        }
+
         return view
+    }
+
+    fun showDialog(){
+        val builder = AlertDialog.Builder(activity)
+        val dialogView = layoutInflater.inflate(R.layout.alarm_dialog, null)
+        val dialogText = dialogView.findViewById<TextView>(R.id.textView_test)
+
+        builder.setView(dialogView)
+            .setPositiveButton("확인"){ dialogInterFace, i ->
+                set_textView.text = dialogText.text
+            }
+            .setNegativeButton("취소", null)
+            .show()
     }
 }
