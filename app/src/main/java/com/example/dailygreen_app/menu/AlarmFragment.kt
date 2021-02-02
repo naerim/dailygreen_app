f0250797b358b6a9c1130d96311c0b37a036d105
Minyoung-Lee <leemy08@naver.com>
2021-02-03 02:34:08 +0900

    [데모]알람 (날짜 정상, 시계 정상)

--- app/src/main/java/com/example/dailygreen_app/menu/AlarmFragment.kt
+++ app/src/main/java/com/example/dailygreen_app/menu/AlarmFragment.kt
@@ -2,19 +2,16 @@ package com.example.dailygreen_app.menu
 
 import android.app.AlarmManager
 import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.content.Context
 import android.os.Bundle
-import android.text.style.UpdateAppearance
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.DatePicker
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.dailygreen_app.R
-import java.text.SimpleDateFormat
-import java.time.Year
 import java.util.*
 
 class AlarmFragment : Fragment(){
@@ -28,92 +25,35 @@ class AlarmFragment : Fragment(){
         // 알람 관리자 소환
         alarmManager = getActivity()!!.getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
-//        var textView = view.findViewById<TextView>(R.id.textView2)
-       val textView: TextView = view.findViewById(R.id.textView2)
-//
-//        textView.text = SimpleDateFormat("dd.MM.yyyy").format(System.currentTimeMillis())
-        //val textView: TextView  = view.findViewById(R.id.show_date)
-        //textView.text = SimpleDateFormat("dd.MM.yyyy").format(System.currentTimeMillis())
+        var textDate: TextView = view.findViewById(R.id.text_date)
+        var textTime: TextView = view.findViewById(R.id.text_time)
 
-        val btnAddAlarm = view.findViewById<Button>(R.id.add_alarm)
-
-//        btnAddAlarm.setOnClickListener {
-//            var calendar = Calendar.getInstance()
-//            var year = calendar.get(Calendar.YEAR)
-//            var month = calendar.get(Calendar.MONTH)
-//            var day = calendar.get(Calendar.DAY_OF_MONTH)
-
-//        val calendar = GregorianCalendar(Locale.KOREA)
-//        val dialog = DatePickerDialog.OnDateSetListener{ view, year, month, dayOfMonth ->
-//            calendar.get(Calendar.YEAR)
-//            calendar.get(Calendar.MONTH)
-//            calendar.get(Calendar.DATE)
-//        }
-
-//            dialog = DatePickerDialog.OnDateSetListener { _, i, i2, i3 ->
-//                textView.text = "${i}년 ${i2 + 1}월 ${i3}일일"
-//            }
- //       }
-
-
-//        fun clickAddalarm(view:View){
-//            // 특정 날짜에 알람 설정하기
-//
-//            // 날짜 선택 다이얼로그 보이기
-//            //val calendar = GregorianCalendar(Locale.KOREA)
-//            var calendar = Calendar.getInstance()
-//            var year = calendar.get(Calendar.YEAR)
-//            var month = calendar.get(Calendar.MONTH)
-//            var day = calendar.get(Calendar.DAY_OF_MONTH)
-//
-//            var listener = DatePickerDialog.OnDateSetListener { _, i, i2, i3 ->
-//                textView.text = "${i}년 ${i2+1}월 ${i3}일"
-//            }
-//        }
+        var btnAddAlarm = view.findViewById<Button>(R.id.add_alarm)
 
 
         var calendar = Calendar.getInstance()
         var year = calendar.get(Calendar.YEAR)
         var month = calendar.get(Calendar.MONTH)
         var day = calendar.get(Calendar.DAY_OF_MONTH)
+        var hour = calendar.get(Calendar.HOUR)
+        var minute = calendar.get(Calendar.MINUTE)
 
+        // 선택 버튼 눌렀을 때
         btnAddAlarm.setOnClickListener {
-            var dialog = DatePickerDialog(context!!, DatePickerDialog.OnDateSetListener { _, i, i2, i3 ->
-                textView.setText("" + day + " " + month + ". " + year)
+            // 시간 선택
+            var picktime = TimePickerDialog(context!!, TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+                textTime.setText("" + hour + "시 " + minute + "분  ")
+            }, hour, minute, true)
+            picktime.show()
+            // 날짜 선택
+            var pickdate = DatePickerDialog(context!!, DatePickerDialog.OnDateSetListener { _, i, i2, i3 ->
+            textDate.setText("" + day + "/ " + month+1 + "/ " + year)
             }, year, month, day)
-            dialog.show()
+
+            pickdate.show()
         }
-
-
         return view
     }
 
 
-//    fun clickAddalarm(view:View){
-//            // 특정 날짜에 알람 설정하기
-//        var textView = view.findViewById<TextView>(R.id.textView2)
-//        var addbutton = view.findViewById<Button>(R.id.add_alarm)
-//            // 날짜 선택 다이얼로그 보이기
-//        var vcontext = activity
-//            //val calendar = GregorianCalendar(Locale.KOREA)
-//        val calendar = GregorianCalendar(Locale.KOREA)
-//        val dialog = DatePickerDialog.OnDateSetListener{ view, year, month, dayOfMonth ->
-//            calendar.get(Calendar.YEAR)
-//            calendar.get(Calendar.MONTH)
-//            calendar.get(Calendar.DATE)
-//
-//            val myFormat = "dd.MM.yyyy" // mention the format you need
-//            val sdf = SimpleDateFormat(myFormat, Locale.US)
-//            textView.text = sdf.format(calendar.time)
-//        }
-//
-//        addbutton.setOnClickListener {
-//            DatePickerDialog(
-//                context!!, dialog,
-//                calendar.get(Calendar.YEAR),
-//                calendar.get(Calendar.MONTH),
-//                calendar.get(Calendar.DAY_OF_MONTH)
-//            ).show()
-//        }
-//    }
 }
