584d24b0ed640ced319d69869e4d71ec5e058ded
naerim <naerim1119@gmail.com>
2021-02-09 15:55:35 +0900

    [FIX] 다이어리 레이아웃 수정, 셋팅 페이지 배경화면

---
+++ app/src/main/java/com/example/dailygreen_app/DiaryDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dailygreen_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DiaryDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diary_detail)
+    }
+}
